6a7e676c0ffa9eb16b2e7cdf86aec2ac3896e1cd
ciccio42 <francescorosa97@virgilio.it>
2025-06-30 16:03:19 +0000

    test script

--- bash/run_test.sh
+++ bash/run_test.sh
@@ -5,16 +5,14 @@ result1=result.log/
 file1=result
 rm -rf result.log/*.*
 
-QUERY_FOLDER=[PATH_TO_FOLDER_THAT_CONTAINS_QUERY_GRAPHS]   # test
-TARGET_FOLDER=[PATH_TO_FOLDER_THAT_CONTAINS_TARGET_GRAPHS] # test
-QUERY_FOLDER=test
-TARGET_FOLDER=test
+QUERY_FOLDER=[PATH_TO_FOLDER_THAT_CONTAINS_QUERY_GRAPHS]
+TARGET_FOLDER=[PATH_TO_FOLDER_THAT_CONTAINS_TARGET_GRAPHS]
 
 # Define the array of query and target graph names
-QUERY_TARGET_NAMES=("[PATH_TO_QUERY_GRAPH] [PATH_TO_QUERY_TARGET_GRAPH]")
+QUERY_TARGET_NAMES=("[NAME_OF_QUERY_GRAPH] [NAME_OF_TARGET_GRAPH]")
 
 # Example of QUERY_TARGET_NAMES:
-QUERY_TARGET_NAMES=("bvg1.sub.grf bvg1.grf" "bvg1_2.sub.grf bvg1.grf" "bvg1_3.sub.grf bvg1.grf" "bvg1_4.sub.grf bvg1.grf" "bvg2.sub.grf bvg2.grf" "bvg3.sub.grf bvg3.grf" "m2d1.sub.grf m2d1.grf" "m2d2.sub.grf m2d2.grf" "rand1.sub.grf rand1.grf" "rand2.sub.grf rand2.grf" "rand3.sub.grf rand3.grf" "rand4.sub.grf rand4.grf" "rnd_ldg1.sub.grf rnd_ldg1.grf" "si2_b03_m400_37.sub.grf si2_b03_m400_37.grf" "si2_b03_m1000_00.sub.grf si2_b03_m1000_00.grf" "si2_b03m_m800_22.sub.grf si2_b03m_m800_22.grf" "si2_b06m_m400_96.sub.grf si2_b06m_m400_96.grf" "si2_b09_m400_37.sub.grf si2_b09_m400_01.grf" "si2_r01_m200_00.sub.grf si2_r01_m200_00.grf" "si2_r01_m1000_30.sub.grf si2_r01_m1000_30.grf" "si2_rnd_eta04_m750_20.sub.grf si2_rnd_eta04_m750_20.grf" "si2_rnd_eta04_m1000_00.sub.grf si2_rnd_eta04_m1000_00.grf")
+# QUERY_TARGET_NAMES=("bvg1.sub.grf bvg1.grf" "bvg1_2.sub.grf bvg1.grf" "bvg1_3.sub.grf bvg1.grf" "bvg1_4.sub.grf bvg1.grf" "bvg2.sub.grf bvg2.grf" "bvg3.sub.grf bvg3.grf" "m2d1.sub.grf m2d1.grf" "m2d2.sub.grf m2d2.grf" "rand1.sub.grf rand1.grf" "rand2.sub.grf rand2.grf" "rand3.sub.grf rand3.grf" "rand4.sub.grf rand4.grf" "rnd_ldg1.sub.grf rnd_ldg1.grf" "si2_b03_m400_37.sub.grf si2_b03_m400_37.grf" "si2_b03_m1000_00.sub.grf si2_b03_m1000_00.grf" "si2_b03m_m800_22.sub.grf si2_b03m_m800_22.grf" "si2_b06m_m400_96.sub.grf si2_b06m_m400_96.grf" "si2_b09_m400_37.sub.grf si2_b09_m400_01.grf" "si2_r01_m200_00.sub.grf si2_r01_m200_00.grf" "si2_r01_m1000_30.sub.grf si2_r01_m1000_30.grf" "si2_rnd_eta04_m750_20.sub.grf si2_rnd_eta04_m750_20.grf" "si2_rnd_eta04_m1000_00.sub.grf si2_rnd_eta04_m1000_00.grf")
 
 for i in "${QUERY_TARGET_NAMES[@]}"; do
     # Split the element into two parts
--- bash/run_test_light.sh
+++ bash/run_test_light.sh
@@ -5,11 +5,16 @@ result1=result.log/
 file1=result
 rm -rf result.log/*.*
 
+QUERY_FOLDER=[PATH_TO_FOLDER_THAT_CONTAINS_QUERY_GRAPHS]
+TARGET_FOLDER=[PATH_TO_FOLDER_THAT_CONTAINS_TARGET_GRAPHS]
+# QUERY_FOLDER=test
+# TARGET_FOLDER=test
+
 # Define the array of query and target graph names
-QUERY_TARGET_NAMES=("[PATH TO QUERY GRAPH] [PATH TO QUERY TARGET GRAPH]")
+QUERY_TARGET_NAMES=("[NAME_OF_QUERY_GRAPH] [NAME_OF_TARGET_GRAPH]")
 
 # Example of QUERY_TARGET_NAMES:
-QUERY_TARGET_NAMES=("bvg1.sub.grf bvg1.grf" "bvg1_2.sub.grf bvg1.grf" "bvg1_3.sub.grf bvg1.grf" "bvg1_4.sub.grf bvg1.grf" "bvg2.sub.grf bvg2.grf" "bvg3.sub.grf bvg3.grf" "m2d1.sub.grf m2d1.grf" "m2d2.sub.grf m2d2.grf" "rand1.sub.grf rand1.grf" "rand2.sub.grf rand2.grf" "rand3.sub.grf rand3.grf" "rand4.sub.grf rand4.grf" "rnd_ldg1.sub.grf rnd_ldg1.grf" "si2_b03_m400_37.sub.grf si2_b03_m400_37.grf" "si2_b03_m1000_00.sub.grf si2_b03_m1000_00.grf" "si2_b03m_m800_22.sub.grf si2_b03m_m800_22.grf" "si2_b06m_m400_96.sub.grf si2_b06m_m400_96.grf" "si2_b09_m400_37.sub.grf si2_b09_m400_01.grf" "si2_r01_m200_00.sub.grf si2_r01_m200_00.grf" "si2_r01_m1000_30.sub.grf si2_r01_m1000_30.grf" "si2_rnd_eta04_m750_20.sub.grf si2_rnd_eta04_m750_20.grf" "si2_rnd_eta04_m1000_00.sub.grf si2_rnd_eta04_m1000_00.grf")
+# QUERY_TARGET_NAMES=("bvg1.sub.grf bvg1.grf" "bvg1_2.sub.grf bvg1.grf" "bvg1_3.sub.grf bvg1.grf" "bvg1_4.sub.grf bvg1.grf" "bvg2.sub.grf bvg2.grf" "bvg3.sub.grf bvg3.grf" "m2d1.sub.grf m2d1.grf" "m2d2.sub.grf m2d2.grf" "rand1.sub.grf rand1.grf" "rand2.sub.grf rand2.grf" "rand3.sub.grf rand3.grf" "rand4.sub.grf rand4.grf" "rnd_ldg1.sub.grf rnd_ldg1.grf" "si2_b03_m400_37.sub.grf si2_b03_m400_37.grf" "si2_b03_m1000_00.sub.grf si2_b03_m1000_00.grf" "si2_b03m_m800_22.sub.grf si2_b03m_m800_22.grf" "si2_b06m_m400_96.sub.grf si2_b06m_m400_96.grf" "si2_b09_m400_37.sub.grf si2_b09_m400_01.grf" "si2_r01_m200_00.sub.grf si2_r01_m200_00.grf" "si2_r01_m1000_30.sub.grf si2_r01_m1000_30.grf" "si2_rnd_eta04_m750_20.sub.grf si2_rnd_eta04_m750_20.grf" "si2_rnd_eta04_m1000_00.sub.grf si2_rnd_eta04_m1000_00.grf")
 
 for i in "${QUERY_TARGET_NAMES[@]}"; do
     # Split the element into two parts
@@ -19,12 +24,16 @@ for i in "${QUERY_TARGET_NAMES[@]}"; do
     # Print the two strings
     echo "Query: $QUERY_NAME - Target string: $TARGET_NAME"
 
-    QUERY_TEST=${QUERY_NAME}
-    TARGET_TEST=${TARGET_NAME}
+    QUERY_TEST=${QUERY_FOLDER}/${QUERY_NAME}
+    TARGET_TEST=${TARGET_FOLDER}/${TARGET_NAME}
 
-    ./bin/vf3l ${QUERY_TEST} ${TARGET_TEST}
+    if [ $1 -eq 1 ]; then
+        gdbserver localhost:1236 bin/vfl ${QUERY_TEST} ${TARGET_TEST}
+    else
+        ./bin/vf3l ${QUERY_TEST} ${TARGET_TEST}
+    fi
 
-    echo $'\n######################################\n'
+    echo $'\n#####################################\n'
 done
 
 cd bash
